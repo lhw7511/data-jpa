a4526f0bebc6ea783ac8e1645aa1af289efcd696
lhw7511 <lhw751@gmail.com>
2022-11-07 10:51:41 +0900

    기존 베이식 jpa 레포지토리 구현

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -4,19 +4,16 @@ package study.datajpa.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberJpaRepository;
 
 @RestController
 @RequiredArgsConstructor
 public class HelloController {
 
-    private final MemberJpaRepository memberJpaRepository;
 
     @GetMapping("/hello")
     public String hello(){
 
-        memberJpaRepository.find(1L);
         return "hello";
     }
 }
--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,10 +1,14 @@
 package study.datajpa.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -21,4 +25,25 @@ public class MemberJpaRepository {
         Member member = em.find(Member.class, id);
         return member;
     }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class,id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+
 }
