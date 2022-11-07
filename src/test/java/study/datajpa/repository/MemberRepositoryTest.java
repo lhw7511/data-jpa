18666f36802dfdd7525cb5172c9cc97721aab56a
lhw7511 <lhw751@gmail.com>
2022-11-07 16:42:06 +0900

    쿼리메소드로 값,dto타입으로 리턴

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     public void 테스트멤버(){
@@ -97,5 +100,31 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findUser("aaa",10);
         assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUsernameList(){
+        Member m1 = new Member("aaa",10);
+        Member m2 = new Member("bbb",20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        usernameList.forEach(n -> System.out.println(n));
+    }
+
+    @Test
+    public void findMemberDto(){
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+        Member m1 = new Member("AAA",10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+        List< MemberDto >  memberDtos= memberRepository.findMemberDto();
+        for(MemberDto dto : memberDtos){
+            System.out.println("dto -> "+dto);
+        }
+
     }
 }
